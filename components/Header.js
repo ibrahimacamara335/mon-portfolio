1a79c229c2aca226a1e56dd840190ddd10ac7fc3
Ibrahima CAMARA <ibrahimacamara335@gmail.com>
2025-04-10 15:00:57 +0000

    Fix: navigation avec Link au lieu de a

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,16 @@
-import styles from './Header.module.css';
+// components/Header.js
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Mon Portfolio</h1>
-      <nav>
-      <a href="/">Accueil</a> | 
-  <a href="/a-propos">À propos</a> | 
-  <a href="/competences">Compétences</a> | 
-  <a href="/projets">Projets</a> | 
-  <a href="/contact">Contact</a>
+      <nav className={styles.nav}>
+        <Link href="/">Accueil</Link>
+        <Link href="/a-propos">À propos</Link>
+        <Link href="/competences">Compétences</Link>
+        <Link href="/projets">Projets</Link>
+        <Link href="/contact">Contact</Link>
       </nav>
     </header>
   );
