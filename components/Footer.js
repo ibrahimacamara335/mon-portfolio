372bcf9fdbdb474c28299b2dbace9cbcb787a542
Ibrahima CAMARA <ibrahimacamara335@gmail.com>
2025-04-10 15:43:22 +0000

    ajouter le nom de developpeur

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} Mon Nom. Tous droits réservés.</p>
+      <p>&copy; {new Date().getFullYear()} Ibrahima Camara. Tous droits réservés.</p>
     </footer>
   );
 }
